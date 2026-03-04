902c170aaeddf61b3cf3f2e2d6282b7733e028d1
David Barbero <dbdstk@gmail.com>
2026-03-04 13:14:13 +0100

    Update stats generation

--- backend/api/stats.js
+++ backend/api/stats.js
@@ -17,24 +17,29 @@ statsRouter
 
     jsonsInDir.forEach(file => {
       const fileData = fs.readFileSync(path.join("./log/gameStats", file));
-      const json = JSON.parse(fileData.toString());
+      try {
+        const json = JSON.parse(fileData.toString());
 
 
-      //Get booster pack distribution
-      json.sets.forEach(set => {
-        if (!boosterPacks[set]) {
-          boosterPacks[set] = 0;
-        }
+        //Get booster pack distribution
+        json.sets.forEach(set => {
+          if (!boosterPacks[set]) {
+            boosterPacks[set] = 0;
+          }
 
-        boosterPacks[set]++;
-      });
+          boosterPacks[set]++;
+        });
 
-      //Get unique player list
-      uniquePlayers.push(...json.cap.filter(({name}) => name !== "bot").map(({id}) => id));
+        //Get unique player list
+        uniquePlayers.push(...json.cap.filter(({name}) => name !== "bot").map(({id}) => id));
 
-      //Get game count
-      if (json.type === "draft") { draftCount++;}
-      if (json.type === "sealed") { sealedCount++;}
+        //Get game count
+        if (json.type === "draft") { draftCount++;}
+        if (json.type === "sealed") { sealedCount++;}
+      } catch (e){
+        console.error("Error processing:", file);
+        console.error(e);
+      }
     });
 
     console.log("Generated stats in " + (Date.now() - startTime) + " ms");
